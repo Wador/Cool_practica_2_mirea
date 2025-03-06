03b85f7e74d50bafb993408106da3065a893a426
Wador <im.alex.nov@gmail.com>
2025-03-06 20:19:53 +0000

    10th commit

---
+++ build/CMakeFiles/console_utility.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/Desktop/sysprog/prac2/src/errors.c" "CMakeFiles/console_utility.dir/src/errors.c.o" "gcc" "CMakeFiles/console_utility.dir/src/errors.c.o.d"
+  "/home/vboxuser/Desktop/sysprog/prac2/src/help.c" "CMakeFiles/console_utility.dir/src/help.c.o" "gcc" "CMakeFiles/console_utility.dir/src/help.c.o.d"
+  "/home/vboxuser/Desktop/sysprog/prac2/src/log.c" "CMakeFiles/console_utility.dir/src/log.c.o" "gcc" "CMakeFiles/console_utility.dir/src/log.c.o.d"
+  "/home/vboxuser/Desktop/sysprog/prac2/src/main.c" "CMakeFiles/console_utility.dir/src/main.c.o" "gcc" "CMakeFiles/console_utility.dir/src/main.c.o.d"
+  "/home/vboxuser/Desktop/sysprog/prac2/src/processes.c" "CMakeFiles/console_utility.dir/src/processes.c.o" "gcc" "CMakeFiles/console_utility.dir/src/processes.c.o.d"
+  "/home/vboxuser/Desktop/sysprog/prac2/src/users.c" "CMakeFiles/console_utility.dir/src/users.c.o" "gcc" "CMakeFiles/console_utility.dir/src/users.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/console_utility.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/console_utility.dir/src/errors.c.o"
+  "CMakeFiles/console_utility.dir/src/errors.c.o.d"
+  "CMakeFiles/console_utility.dir/src/help.c.o"
+  "CMakeFiles/console_utility.dir/src/help.c.o.d"
+  "CMakeFiles/console_utility.dir/src/log.c.o"
+  "CMakeFiles/console_utility.dir/src/log.c.o.d"
+  "CMakeFiles/console_utility.dir/src/main.c.o"
+  "CMakeFiles/console_utility.dir/src/main.c.o.d"
+  "CMakeFiles/console_utility.dir/src/processes.c.o"
+  "CMakeFiles/console_utility.dir/src/processes.c.o.d"
+  "CMakeFiles/console_utility.dir/src/users.c.o"
+  "CMakeFiles/console_utility.dir/src/users.c.o.d"
+  "console_utility"
+  "console_utility.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/console_utility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
